93dc597cc7f906d8fdee0a50fb2b6f37cd52330b
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-08-21 10:35:25 +0700

    add example build binary

---
+++ cmd/buildmain.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+env GOOS=windows GOARCH=386 go build main.go
---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+/*
+  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+
+	fmt.Println("Binary Build Success!!")
+}
+
